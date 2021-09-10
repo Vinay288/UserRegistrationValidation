f9b556288f70a3c40eae3b189bcbc682b108d332
Vinay288 <vinayhiremath288@gmail.com>
2021-09-10 20:22:11 +0530

    MODIFY: added try catch blocks for password tests to catch exception

--- test/java/UserRegistrationWithValidationTest/UserRegistrationTest.java
+++ test/java/UserRegistrationWithValidationTest/UserRegistrationTest.java
@@ -3,76 +3,82 @@ package UserRegistrationWithValidationTest;
 
 import org.junit.Test;
 
+import UserRegistrationWithValidationTest.PasswordValidationException.PasswordExceptionType;
+
 import org.junit.Assert;
 
 public class UserRegistrationTest {
 	@Test
 	public void givenFirstName_WhenProper_ShouldReturnTrue() {
 		UserRegistrationValidator validator = new UserRegistrationValidator();
-		boolean result = validator.validateName("Vinay");
+		boolean result = validator.validateFirstName("Vinay");
 		Assert.assertEquals(true, result);
 	}
 
+//
+//	public void givenFirstName_WhenNull_ShouldThrowException() {
+//		
+//	}
 	@Test
 	public void givenFirstName_WhenShort_ShouldReturnFalse() {
 		UserRegistrationValidator validator = new UserRegistrationValidator();
-		boolean result = validator.validateName("Vi");
+		boolean result = validator.validateFirstName("Vi");
 		Assert.assertEquals(false, result);
 	}
 
 	@Test
 	public void givenFirstName_WhenStartWithSpecialChars_ShouldReturnFalse() {
 		UserRegistrationValidator validator = new UserRegistrationValidator();
-		boolean result = validator.validateName("$Vinay");
+		boolean result = validator.validateFirstName("$Vinay");
 		Assert.assertEquals(false, result);
 	}
 
 	@Test
 	public void givenFirstName_WhenStartWithSmallLetter_ShouldReturnFalse() {
 		UserRegistrationValidator validator = new UserRegistrationValidator();
-		boolean result = validator.validateName("vinay");
+		boolean result = validator.validateFirstName("vinay");
 		Assert.assertEquals(false, result);
 	}
 
 	@Test
 	public void givenFirstName_WhenLongAndStartWithCapital_ShouldReturnTrue() {
 		UserRegistrationValidator validator = new UserRegistrationValidator();
-		boolean result = validator.validateName("Vinayshhshsh");
+		boolean result = validator.validateFirstName("Vinayshhshsh");
 		Assert.assertEquals(true, result);
 	}
 
 	@Test
 	public void givenLastName_WhenProper_ShouldReturnTrue() {
 		UserRegistrationValidator validator = new UserRegistrationValidator();
-		boolean result = validator.validateName("Hirem");
+		boolean result = validator.validateFirstName("Hirem");
 		Assert.assertEquals(true, result);
 	}
 
 	@Test
 	public void givenLastName_WhenShort_ShouldReturnFalse() {
 		UserRegistrationValidator validator = new UserRegistrationValidator();
-		boolean result = validator.validateName("Hi");
+		boolean result = validator.validateFirstName("Hi");
 		Assert.assertEquals(false, result);
 	}
 
 	@Test
 	public void givenLastName_WhenStartWithSpecialChars_ShouldReturnFalse() {
 		UserRegistrationValidator validator = new UserRegistrationValidator();
-		boolean result = validator.validateName("$Vinay");
+		boolean result = validator.validateFirstName("$Vinay");
 		Assert.assertEquals(false, result);
 	}
 
 	@Test
 	public void givenLastName_WhenStartWithSmallLetter_ShouldReturnFalse() {
 		UserRegistrationValidator validator = new UserRegistrationValidator();
-		boolean result = validator.validateName("hiremath");
+		boolean result = validator.validateFirstName("hiremath");
 		Assert.assertEquals(false, result);
 	}
 
 	@Test
 	public void givenLastName_WhenLongAndStartWithCapital_ShouldReturnTrue() {
 		UserRegistrationValidator validator = new UserRegistrationValidator();
-		boolean result = validator.validateName("Vinayshhshsh");
+		boolean result = validator.validateFirstName("Vinayshhshsh");
 		Assert.assertEquals(true, result);
 	}
 
@@ -145,47 +151,81 @@ public class UserRegistrationTest {
 		boolean result = validator.phoneValidation("919110473394");
 		Assert.assertEquals(false, result);
 	}
-	
+
 	@Test
 	public void givenPhoneNumber_WhenLengthIsMore_ShouldReturnFalse() {
 		UserRegistrationValidator validator = new UserRegistrationValidator();
 		boolean result = validator.phoneValidation("91 911047339454");
 		Assert.assertEquals(false, result);
 	}
-	
+
 	@Test
-	public void givenPassword_WhenProper_ShouldReturnTrue() {
+	public void givenPassword_WhenProper_ShouldReturnTrue() throws PasswordValidationException {
 		UserRegistrationValidator validator = new UserRegistrationValidator();
 		boolean result = validator.passwordValidation("A1@abcdefgh");
 		Assert.assertEquals(true, result);
 	}
-	
+
 	@Test
-	public void givenPassword_WhenNoCapitalLetter_ShouldReturnFalse() {
-		UserRegistrationValidator validator = new UserRegistrationValidator();
-		boolean result = validator.passwordValidation("1@abcdefgh");
-		Assert.assertEquals(false, result);
+	public void givenPassword_WhenNoCapitalLetter_ShouldThrowInvalidException() throws PasswordValidationException {
+		try {
+			UserRegistrationValidator validator = new UserRegistrationValidator();
+			boolean result = validator.passwordValidation("1@abcdefgh");
+		} catch (PasswordValidationException e) {
+			Assert.assertEquals(PasswordExceptionType.PASSWORD_INVALID, e.type);
+		}
 	}
-	
+
 	@Test
-	public void givenPassword_WhenNoNumberLetter_ShouldReturnFalse() {
-		UserRegistrationValidator validator = new UserRegistrationValidator();
-		boolean result = validator.passwordValidation("A@abcdefgh");
-		Assert.assertEquals(false, result);
+	public void givenPassword_WhenNoNumberLetter_ShouldThrowInvalidException() throws PasswordValidationException {
+		try {
+			UserRegistrationValidator validator = new UserRegistrationValidator();
+			boolean result = validator.passwordValidation("A@abcdefgh");
+		} catch (PasswordValidationException e) {
+			Assert.assertEquals(PasswordExceptionType.PASSWORD_INVALID, e.type);
+		}
 	}
-	
+
 	@Test
-	public void givenPassword_WhenNoSpecialLetter_ShouldReturnFalse() {
-		UserRegistrationValidator validator = new UserRegistrationValidator();
-		boolean result = validator.passwordValidation("1Aabcdefgh");
-		Assert.assertEquals(false, result);
+	public void givenPassword_WhenNoSpecialLetter_ShouldThrowInvalidException() throws PasswordValidationException {
+		try {
+			UserRegistrationValidator validator = new UserRegistrationValidator();
+			boolean result = validator.passwordValidation("1Aabcdefgh");
+		} catch (PasswordValidationException e) {
+			Assert.assertEquals(PasswordExceptionType.PASSWORD_INVALID, e.type);
+		}
 	}
-	
+
 	@Test
-	public void givenPassword_WhenLessThenEightCharacters_ShouldReturnFalse() {
-		UserRegistrationValidator validator = new UserRegistrationValidator();
-		boolean result = validator.passwordValidation("1A@ab");
-		Assert.assertEquals(false, result);
+	public void givenPassword_WhenLessThenEightCharacters_ShouldThrowInvalidException()
+			throws PasswordValidationException {
+		try {
+			UserRegistrationValidator validator = new UserRegistrationValidator();
+			boolean result = validator.passwordValidation("1A@ab");
+		} catch (PasswordValidationException e) {
+			Assert.assertEquals(PasswordExceptionType.PASSWORD_INVALID, e.type);
+		}
+
+	}
+
+	@Test
+	public void givenPassword_WhenNull_ShouldThrowException() throws PasswordValidationException {
+		try {
+			UserRegistrationValidator validator = new UserRegistrationValidator();
+			boolean result = validator.passwordValidation(null);
+		} catch (PasswordValidationException e) {
+			Assert.assertEquals(PasswordExceptionType.PASSWORD_NULL, e.type);
+		}
+	}
+
+	@Test
+	public void givenPassword_WhenEmpty_ShouldThrowException() throws PasswordValidationException {
+		try {
+			UserRegistrationValidator validator = new UserRegistrationValidator();
+			boolean result = validator.passwordValidation("");
+		} catch (PasswordValidationException e) {
+			Assert.assertEquals(PasswordExceptionType.PASSWORD_EMPTY, e.type);
+		}
 	}
 
 }
